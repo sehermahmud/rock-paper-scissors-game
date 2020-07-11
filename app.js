ae1bd2f259d3658a51c70317a0cc8a35d0235511
sehermahmud <seher.mahmud@gmail.com>
2020-07-12 04:14:36 +0600

    Added some changes

--- app.js
+++ app.js
@@ -46,18 +46,6 @@ const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) =>
     ? RESULT_PLAYER_WINS
     : RESULT_COMPUTER_WINS;
 
-// if (cChoice === pChoice) {
-//   return RESULT_DRAW;
-// } else if (
-//   (cChoice === ROCK && pChoice === PAPER) ||
-//   (cChoice === PAPER && pChoice === SCISSORS) ||
-//   (cChoice === SCISSORS && pChoice === ROCK)
-// ) {
-//   return RESULT_PLAYER_WINS;
-// } else {
-//   return RESULT_COMPUTER_WINS;
-// }
-
 startGameBtn.addEventListener('click', () => {
   if (gameIsRunning) {
     return;
@@ -86,64 +74,3 @@ startGameBtn.addEventListener('click', () => {
   gameIsRunning = false;
 });
 
-// it is not related to game!!
-
-// const combine = (resultHandler, operation, ...numbers) => {
-//   const validateNumber = number => {
-//     return isNaN(number) ? 0 : number;
-//   };
-
-//   let sum = 0;
-//   for (const num of numbers) {
-//     if (operation === 'ADD') {
-//       sum += validateNumber(num);
-//     } else {
-//       sum -= validateNumber(num);
-//     }
-//   }
-//   resultHandler(sum);
-// };
-
-// const subtractUp = function(resultHandler, ...numbers) {
-//   let sum = 0;
-//   for (const num of numbers) {
-//     // don't use that
-//     sum -= num;
-//   }
-//   resultHandler(sum, 'The result after adding all numbers is');
-// };
-
-// const showResult = (messageText, result) => {
-//   alert(messageText + ' ' + result);
-// };
-
-// combine(
-//   showResult.bind(this, 'The result after adding all numbers is:'),
-//   'ADD',
-//   1,
-//   5,
-//   'fdsa',
-//   -3,
-//   6,
-//   10
-// );
-// combine(
-//   showResult.bind(this, 'The result after adding all numbers is:'),
-//   'ADD',
-//   1,
-//   5,
-//   10,
-//   -3,
-//   6,
-//   10,
-//   25,
-//   88
-// );
-// combine(
-//   showResult.bind(this, 'The result after subtracting all numbers is:'),
-//   'SUBTRACT',
-//   1,
-//   10,
-//   15,
-//   20
-// );
